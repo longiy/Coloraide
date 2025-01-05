b7104d44d5d213caf5d7401088a482682ffe1078
longiy <46914861+longiy@users.noreply.github.com>
2025-01-06 03:39:46 +0800

    Lab sldier fix

---
+++ utils/__init__.py
@@ -0,0 +1,4 @@
+# In utils/__init__.py
+from .color_conversions import rgb_to_lab, lab_to_rgb
+
+__all__ = ['rgb_to_lab', 'lab_to_rgb']
--- utils/color_conversions.py
+++ utils/color_conversions.py
@@ -11,7 +11,7 @@ def rgb_to_xyz(rgb):
     g = to_linear(g)
     b = to_linear(b)
     
-    # Linear RGB to XYZ
+    # Linear RGB to XYZ using D65 white point
     x = r * 0.4124564 + g * 0.3575761 + b * 0.1804375
     y = r * 0.2126729 + g * 0.7151522 + b * 0.0721750
     z = r * 0.0193339 + g * 0.1191920 + b * 0.9503041
@@ -20,43 +20,51 @@ def rgb_to_xyz(rgb):
 
 def xyz_to_lab(xyz):
     x, y, z = xyz
-    # D65 illuminant
+    # D65 white point
     ref_x, ref_y, ref_z = 0.95047, 1.0, 1.08883
     
+    # XYZ to relative XYZ
     x = x / ref_x
     y = y / ref_y
     z = z / ref_z
     
-    def f(t):
-        delta = 6.0 / 29.0
-        if t > delta ** 3:
-            return t ** (1.0 / 3.0)
-        return t / (3.0 * delta ** 2) + 4.0 / 29.0
+    # Relative XYZ to Lab
+    epsilon = 0.008856
+    kappa = 903.3
     
+    def f(t):
+        if t > epsilon:
+            return t ** (1/3)
+        return (kappa * t + 16) / 116
+
     fx = f(x)
     fy = f(y)
     fz = f(z)
     
-    L = 116.0 * fy - 16.0
-    a = 500.0 * (fx - fy)
-    b = 200.0 * (fy - fz)
+    L = max(0, min(100, 116 * fy - 16))
+    a = max(-128, min(127, 500 * (fx - fy)))
+    b = max(-128, min(127, 200 * (fy - fz)))
     
     return (L, a, b)
 
 def lab_to_xyz(lab):
     L, a, b = lab
-    # D65 illuminant
+    
+    # D65 white point
     ref_x, ref_y, ref_z = 0.95047, 1.0, 1.08883
     
-    fy = (L + 16.0) / 116.0
-    fx = a / 500.0 + fy
-    fz = fy - b / 200.0
+    epsilon = 0.008856
+    kappa = 903.3
+    
+    fy = (L + 16) / 116
+    fx = fy + (a / 500)
+    fz = fy - (b / 200)
     
     def f_inv(t):
-        delta = 6.0 / 29.0
-        if t > delta:
-            return t ** 3
-        return 3 * delta ** 2 * (t - 4.0 / 29.0)
+        t3 = t ** 3
+        if t3 > epsilon:
+            return t3
+        return (116 * t - 16) / kappa
     
     x = ref_x * f_inv(fx)
     y = ref_y * f_inv(fy)
@@ -66,32 +74,41 @@ def lab_to_xyz(lab):
 
 def xyz_to_rgb(xyz):
     x, y, z = xyz
+    
     # XYZ to linear RGB
-    r = x *  3.2404542 + y * -1.5371385 + z * -0.4985314
-    g = x * -0.9692660 + y *  1.8760108 + z *  0.0415560
-    b = x *  0.0556434 + y * -0.2040259 + z *  1.0572252
+    r = x * 3.2404542 - y * 1.5371385 - z * 0.4985314
+    g = -x * 0.9692660 + y * 1.8760108 + z * 0.0415560
+    b = x * 0.0556434 - y * 0.2040259 + z * 1.0572252
     
     # Linear RGB to sRGB
     def to_srgb(c):
-        if c > 0.0031308:
-            return 1.055 * (c ** (1.0 / 2.4)) - 0.055
-        return 12.92 * c
+        if c <= 0.0031308:
+            return max(0, min(1, 12.92 * c))
+        return max(0, min(1, 1.055 * (c ** (1/2.4)) - 0.055))
     
     r = to_srgb(r)
     g = to_srgb(g)
     b = to_srgb(b)
     
-    # Clamp values
-    r = max(0, min(1, r))
-    g = max(0, min(1, g))
-    b = max(0, min(1, b))
-    
     return (r, g, b)
 
+# Add these two new functions:
 def rgb_to_lab(rgb):
+    """Convert from RGB to LAB directly"""
     xyz = rgb_to_xyz(rgb)
     return xyz_to_lab(xyz)
 
 def lab_to_rgb(lab):
+    """Convert from LAB to RGB directly"""
     xyz = lab_to_xyz(lab)
     return xyz_to_rgb(xyz)
+
+# Don't forget to add them to __all__
+__all__ = [
+    'rgb_to_xyz',
+    'xyz_to_lab',
+    'lab_to_xyz',
+    'xyz_to_rgb',
+    'rgb_to_lab',  # new
+    'lab_to_rgb'   # new
+]
